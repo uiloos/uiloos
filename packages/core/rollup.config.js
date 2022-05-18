d150398e33b4d4367117bffca0d6402c9d2c3b9a
Maarten Hus <maartenhus@gmail.com>
2022-05-18 15:14:30 +0200

    fix: typings now include comments
    Without comments there are no doc comments as well. Without doc
    comments autocomplete in editors miss this information in their
    intellisense popups.
    
    Fixed by adding setting `removeComments` to `false` for the React, and
    Vue packages. The Angular package was already properly configured.
    
    The `core` package needed more work, because is contains massive
    comments only needed for development. The fix is generating the build
    twice, once with comments and once without. This way the `code` does
    not include any comments, but the typings do.

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -7,39 +7,56 @@ import { terser } from 'rollup-plugin-terser';
 
 const packageJson = require('./package.json');
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-        name: '@uiloos/core',
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-      {
-        file: `dist/uiloos-core-${packageJson.version}-min.js`,
-        format: 'iife',
-        name: 'uiloos',
-        plugins: [terser()],
-      },
-    ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-    ],
-  },
-  {
+console.log('Building in mode', process.env.BUILD_MODE);
+
+/*
+  Mode is needed so typings are generated with comments intact or
+  without any comments. A bit annoying see: 
+
+  https://github.com/microsoft/TypeScript/issues/14619
+*/
+const isCodeMode = process.env.BUILD_MODE === 'code';
+
+const result = [];
+
+result[0] = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: '@uiloos/core',
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: `dist/uiloos-core-${packageJson.version}-min.js`,
+      format: 'iife',
+      name: 'uiloos',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: isCodeMode ? './tsconfig.json' : './tsconfig.typings.json',
+    }),
+  ],
+};
+
+if (!isCodeMode) {
+  result[1] = {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     external: [/\.css$/],
     plugins: [dts()],
-  },
-];
+  };
+}
+
+export default result;
